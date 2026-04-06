55babfcc30d50686bf5ce9f7669507a169209d0a
Ahmed Sherif <ahmed.sherif15794@gmail.com>
2026-04-06 20:18:23 +0200

    style: style the try again button

--- lib/core/api/api_constants.dart
+++ lib/core/api/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "http://api.weatherapi.com/v1";
+  static const String baseUrl = "https://api.weatherapi.com/v1";
   static const String apiKey = "03cf6082dff045bc86f91653252908";
   static const String forcastEndPoint = "/forecast.json";
 }
